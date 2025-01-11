9a5434e6583d45c3996c6fb7f9bff7c350bdec1a
Dheiver Francisco Santos <165256830+dheiver2@users.noreply.github.com>
2025-01-11 18:49:35 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
-// next.config.js
 const path = require('path');
 
 module.exports = {
   webpack: (config, { isServer }) => {
-    // Adiciona o node-loader para arquivos .node
     config.module.rules.push({
       test: /\.node$/,
       use: 'node-loader',
     });
-
-    // Retorna a configuração atualizada
     return config;
   },
 };
